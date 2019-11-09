bfe725ff0c4a76a3523766f16c91411b54b2ca83
Kaktuszon <kaktus@kaktuszon.org>
2019-11-10 00:18:28 +0100

    Added a function for collision with duck

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
+let screen_width = 200;
+let screen_height = 200;
+
 let canScore = 1;
 let score = 0;
 
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(screen_width, screen_height);
     frameRate(30);
-    duckImage = loadImage('duck.png');
     soundFormats('mp3', 'ogg');
+    
     duckSound = loadSound('quack.ogg');
+    duckImage = loadImage('duck.png');
+    
     document.getElementById("score").innerHTML = score;
 }
 
@@ -14,12 +19,15 @@ function draw() {
     background(137, 207, 240);
     image(duckImage, duckX, duckY);
 
+    checkCollision();
+}
+
+function checkCollision() {
     if((mouseX > duckX && mouseX < duckHeadx) && (mouseY > duckY && mouseY < duckHeady)) {
         textSize(16);
-        text('quack', duckX-32, duckY-16);
+        text('quack!', duckX-32, duckY-16);
         scoring();
         canScore = 0;
-        
     } else {
         canScore = 1;
     }
