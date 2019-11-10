90ea0b5ebc40e313213e58ce54ecdcbc1adc47c6
Kaktuszon <kaktus@kaktuszon.org>
2019-11-10 17:24:49 +0100

    Added width and height for duck and head

--- duck.js
+++ duck.js
@@ -1,7 +1,12 @@
 let duckImage;
 let duckSound;
 
-let duckX = (screen_width/2)-32;
-let duckY = (screen_height/2)-32;
-let duckHeadx = duckX + 32;
-let duckHeady = duckY + 16;
+let duckW = 134;
+let duckH = 90;
+let duckX = (screen_width/2)-(duckW/2);
+let duckY = (screen_height/2)-(duckH/2);
+
+let duckHeadW = 72;
+let duckHeadH = 32;
+let duckHeadx = duckX + duckHeadW;
+let duckHeady = duckY + duckHeadH;
