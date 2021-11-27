2c1dccf0a1694d37d7ce6b549cd70bb02c61e4a8
Kaktuszon <kaktus@kaktuszon.org>
2021-11-27 08:28:03 +0100

    Changed variables for duck to new class

--- game.js
+++ game.js
@@ -3,29 +3,35 @@ let screen_height = 300;
 
 let canScore = 1;
 let score = 0;
+let duck;    
 
 function setup() {
     createCanvas(screen_width, screen_height);
     frameRate(30);
     soundFormats('mp3', 'ogg');
+
+    duck = new Duck(100, 100);
+
+    duck.image = loadImage('duck.png');
+    duck.sound = loadSound('quack.ogg');
     
-    duckSound = loadSound('quack.ogg');
-    duckImage = loadImage('duck.png');
+    //duckSound = loadSound('quack.ogg');
+    //duckImage = loadImage('duck.png');
     
     document.getElementById("score").innerHTML = score;
 }
 
 function draw() {
     background(137, 207, 240);
-    image(duckImage, duckX, duckY);
+    image(duck.image, duck.x, duck.y);
 
     checkCollision();
 }
 
 function checkCollision() {
-    if((mouseX > duckX && mouseX < duckHeadx) && (mouseY > duckY && mouseY < duckHeady)) {
+    if((mouseX > duck.x && mouseX < duck.headX) && (mouseY > duck.y && mouseY < duck.headY)) {
         textSize(16);
-        text('quack!', duckX-16, duckY-16);
+        text('quack!', duck.x-16, duck.y-16);
         scoring();
         canScore = 0;
     } else {
@@ -35,8 +41,8 @@ function checkCollision() {
 
 function scoring() {
     if(canScore == 1) {
-        duckSound.setVolume(0.5);
-        duckSound.play();
+        duck.sound.setVolume(0.5);
+        duck.sound.play();
         score++;
         document.getElementById("score").innerHTML = score;
     }
